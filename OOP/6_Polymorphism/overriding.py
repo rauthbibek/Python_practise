3c98fe4d371c999bafbdf2da8ca91e0b89e9894a
Bibek Rauth <rauth.bibek007@gmail.com>
2022-02-13 16:10:03 +0530

    updated

--- OOP/6_Polymorphism/overriding.py
+++ OOP/6_Polymorphism/overriding.py
@@ -1,36 +1,25 @@
 from math import pi
 class Shape:
-
     def __init__(self,name):
         self.name = name
-
     def area(self):
         pass
-
     def __str__(self):
         return self.name
-
     def fact(self):
         print("It's two dimensional shape")
-
 class Square(Shape):
-
     def __init__(self,length):
         super().__init__("Square")
         self.length = length
-
     def area(self):
         return self.length**2
-
     def fact(self):
         print("It's a Square")
-
 class Circle(Shape):
-
     def __init__(self,radius):
         super().__init__("Circle")
         self.radius = radius
-
     def area(self):
         return pi*self.radius**2
 
